f43ce114694db76c60f7467640fe587457814151
dantehurr <75588090+dantehurr@users.noreply.github.com>
2024-01-03 15:09:11 -0500

    Updated Projects.tsx
    Finished card container element

--- src/pages/components/Projects.tsx
+++ src/pages/components/Projects.tsx
@@ -2,38 +2,37 @@ const Projects = () => {
     return (
         <section className="section">
             <header className="section-header">My Work</header>
-            <div>
-                Projects
-            </div>
-            <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
 
-            <div className="card-container bg-gray-200 dashed-hover block">
-                    <a href="#" className="card">
-                        <div className="card-content bg-slate-500">
-                            <img className="" src="https://picsum.photos/200" />
-                            <div className="card-header">
-                                <h4>h4</h4>
-                            </div>
+            <div className="card-container">
+                <div className="bg-blue-500 card dashed-hover">
+                    <a href="#">
+                        <img className="card-image" src="https://picsum.photos/400" />
+                        <div className="p-4">
+                            <h4 className="card-header">Header</h4>
+                            <p className="card-content">Para</p>
+                            <p className="card-subtitle">tools</p>
                         </div>
                     </a>
                 </div>
 
-                <div className="card-container bg-gray-200 dashed-hover">
-                    <a href="#" className="card">
-                        <div className="card-content bg-slate-500">
-                            <div className="card-header">
-                                <h4>h4</h4>
-                            </div>
+                <div className="bg-blue-500 card dashed-hover">
+                    <a href="#">
+                        <img className="card-image" src="https://picsum.photos/400" />
+                        <div className="p-4">
+                            <h4 className="card-header">Header</h4>
+                            <p className="card-content">Para</p>
+                            <p className="card-subtitle">tools</p>
                         </div>
                     </a>
                 </div>
 
-                <div className="card-container bg-gray-200 dashed-hover">
-                    <a href="#" className="card">
-                        <div className="card-content bg-slate-500">
-                            <div className="card-header">
-                                <h4>h4</h4>
-                            </div>
+                <div className="bg-blue-500 card dashed-hover">
+                    <a href="#">
+                        <img className="card-image" src="https://picsum.photos/400" />
+                        <div className="p-4">
+                            <h4 className="card-header">Header</h4>
+                            <p className="card-content">Para</p>
+                            <p className="card-subtitle">tools</p>
                         </div>
                     </a>
                 </div>
